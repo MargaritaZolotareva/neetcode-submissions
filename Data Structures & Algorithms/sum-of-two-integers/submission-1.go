dd79282e7004104e379c141196c13b6c466c329d
Margarita Zolotareva <mrg.zolotareva@gmail.com>
2026-05-15 14:27:40 +0500

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.go
@@ -0,0 +1,25 @@
+func getSum(a int, b int) int {
+	for b != 0 {
+		carry := (a & b) << 1
+		a = a ^ b
+		b = carry
+	}
+
+	return a
+}
+
+
+
+
+
+
+
+
+
+	// for carry != 0 {
+	// 	carry := (a & b) << 1
+	// 	a = a ^ b
+	// 	b = carry
+	// }
+
+	// return a
